5fd42a472d744deb48f8026b238aa58ace09c633
raimnascimento <raissa.mnascimento26@gmail.com>
2026-05-10 19:34:15 -0300

    ajuste das imagens e layout

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,6 +3,7 @@ var usuarioModel = require("../models/usuarioModel");
 function autenticar(req, res) {
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var erro = false;
 
     if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
